e81f6c443547153c15da4b3c7c5621a3b0a8daf1
Золин Никита <zolinnikita69@gmail.com>
2023-05-09 15:35:06 +0500

    Добавьте файлы проекта.

---
+++ pz 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string S = "lol";
+char C = 'o';
+System.Text.StringBuilder T = new StringBuilder();
+CharEnumerator N = S.GetEnumerator();
+while (N.MoveNext())
+{
+    char i = N.Current;
+    T.Append(i);
+    if (i == C) T.Append(i);
+}
+S = T.ToString();
+Console.WriteLine(S);
